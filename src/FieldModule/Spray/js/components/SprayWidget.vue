6c969bff9cf682d0fc7266bee4a1fd85bef0bc7e
Jamie Gaehring <gaehj457@gmail.com>
2020-04-16 14:29:22 -0400

    Lay out & style SprayWidget.

--- src/FieldModule/Spray/js/components/SprayWidget.vue
+++ src/FieldModule/Spray/js/components/SprayWidget.vue
@@ -1,9 +1,51 @@
 <template>
-  <div>
-      <p
-        @click="openSpraying">
-        Record Spraying Activity
-      </p>
+  <div id="spray-widget-container">
+    <div class="fade-left"></div>
+    <div class="fade-right"></div>
+    <div class="spray-card-container">
+      <div class="scroll-x" :style="scrollStyle">
+        <farm-card
+          class="spray-card"
+          boxShadow="-1px 2px 6px rgba(0, 0, 0, 0.25)"
+          width="12rem"
+          space=".75rem"
+          marginLeft="1rem">
+          <p class="name">
+            Pre Emerge - North Field - 2020 Corn
+          </p>
+          <p class="date">
+            June 10, 2020
+          </p>
+        </farm-card>
+        <farm-card
+          class="spray-card"
+          boxShadow="-1px 2px 6px rgba(0, 0, 0, 0.25)"
+          width="12rem"
+          space=".75rem">
+          <p class="name">
+            Post Emerge - North Field - 2020 Corn
+          </p>
+          <p class="date">
+            June 28, 2020
+          </p>
+        </farm-card>
+        <farm-card
+          class="spray-card"
+          boxShadow="-1px 2px 6px rgba(0, 0, 0, 0.25)"
+          width="12rem"
+          space=".75rem">
+          <p class="name">
+            Post Emerge - East Field - 2020 Beans
+          </p>
+          <p class="date">
+            June 28, 2020
+          </p>
+        </farm-card>
+      </div>
+    </div>
+    <div class="btn-container">
+      <button class="btn btn-info">Show all</button>
+    </div>
   </div>
 </template>
 
@@ -11,15 +53,89 @@
 export default {
   name: 'SprayWidget',
   props: ['logs'],
+  data() {
+    return {
+      scrollStyle: {
+        paddingBottom: '15px',
+      },
+    };
+  },
+  mounted() {
+    this.calcScrollStyle();
+  },
   methods: {
-    openSpraying(e) {
-      e.stopPropagation();
-      this.$router.push({ path: '/spray' });
+    // This calculates the exact padding needed to hide the scrollbar, because
+    // it can vary between browsers.
+    calcScrollStyle() {
+      const scrollX = document.querySelector('.scroll-x');
+      const paddingBottom = `calc(1rem + ${scrollX.offsetHeight - scrollX.clientHeight}px)`;
+      this.scrollStyle = { paddingBottom };
     },
   },
 };
 </script>
 
 <style scoped>
+#spray-widget-container {
+  position: relative;
+}
+.fade-left {
+  position: absolute;
+  top: 0px;
+  left: -1rem;
+  height: 100%;
+  width: 1rem;
+  z-index: 10;
+  background: linear-gradient(to left, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 50%);
+}
+.fade-right {
+  position: absolute;
+  top: 0px;
+  right: -1rem;
+  height: 100%;
+  width: 1rem;
+  z-index: 10;
+  background: linear-gradient(to right, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1) 50%);
+}
 
+.spray-card-container {
+  white-space: nowrap;
+  overflow: hidden;
+  padding: .5rem 1rem;
+  margin-left: -1rem;
+  margin-right: -1rem;
+  height: 6.25rem;
+}
+.scroll-x {
+  overflow-x: scroll;
+  overflow-y: hidden;
+  box-sizing: content-box;
+  height: 100%;
+  margin-right: -1rem;
+  margin-left: -1rem;
+  padding-left: 1rem;
+}
+.spray-card {
+  display: inline-block;
+  white-space: normal;
+  margin-right: 1rem;
+  margin-top: .5rem;
+}
+
+p {
+  margin-bottom: .125rem;
+  line-height: 1rem;
+}
+.name {
+  font-weight: bold;
+  font-size: .875rem;
+}
+.date {
+  font-size: .625rem;
+  text-transform: uppercase;
+}
+
+.btn-container {
+  padding-top: .5rem;
+}
 </style>
