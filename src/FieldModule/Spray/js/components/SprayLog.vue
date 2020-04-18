499d897a12112ace7f9e75019002990b79a8e393
Jamie Gaehring <gaehj457@gmail.com>
2020-04-18 15:22:57 -0400

    Add tabs to SprayLog.

--- src/FieldModule/Spray/js/components/SprayLog.vue
+++ src/FieldModule/Spray/js/components/SprayLog.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>Spray log screen</div>
+  <div id="spray-log-screen">
+
+    <farm-tabs :tabs="['General', 'Components']">
+
+      <template #general>
+        <farm-tiles :columns="1" :space="'0'">
+          <farm-card>
+            Main Info
+          </farm-card>
+        </farm-tiles>
+      </template>
+
+      <template #components>
+        <farm-tiles :columns="1" :space="'0'">
+          <farm-card>
+            Components
+          </farm-card>
+        </farm-tiles>
+      </template>
+
+    </farm-tabs>
+
+  </div>
 </template>
 
 <script>
@@ -9,5 +31,8 @@ export default {
 </script>
 
 <style scoped>
-
+#spray-log-screen {
+  background-color: #eee;
+  height: calc(100vh - 3rem);
+}
 </style>
