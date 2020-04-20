54a96282cc2d73c4983c2442a7eaf456ccf9bc8e
Jamie Gaehring <gaehj457@gmail.com>
2020-04-20 15:29:41 -0400

    Add main info to SprayLog.

--- src/FieldModule/Spray/js/components/SprayLog.vue
+++ src/FieldModule/Spray/js/components/SprayLog.vue
@@ -4,19 +4,23 @@
     <farm-tabs :tabs="['General', 'Components']">
 
       <template #general>
-        <farm-tiles :columns="1" :space="'0'">
-          <farm-card>
-            Main Info
-          </farm-card>
-        </farm-tiles>
+        <div class="container-fluid">
+          <farm-tiles
+            :columns="[1, 2, 3]"
+            :breakpoints="[0, 600, 900]"
+            :space="['0', '1rem']">
+            <farm-log-main-info
+              :name="'Pre Emerge - North Field - Corn 2020'"
+              :done="false"
+              :timestamp="'June 10, 2020'"/>
+          </farm-tiles>
+        </div>
       </template>
 
       <template #components>
-        <farm-tiles :columns="1" :space="'0'">
-          <farm-card>
-            Components
-          </farm-card>
-        </farm-tiles>
+        <div class="container-fluid">
+          Components
+        </div>
       </template>
 
     </farm-tabs>
@@ -35,4 +39,18 @@ export default {
   background-color: #eee;
   height: calc(100vh - 3rem);
 }
+
+.container-fluid {
+  max-width: 1200px;
+  margin: auto;
+  background-color: #eee;
+  padding: 0;
+}
+
+@media (min-width: 600px) {
+  .container-fluid {
+    padding: 1rem;
+  }
+}
+
 </style>
